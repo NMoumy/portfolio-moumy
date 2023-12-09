42620fbe26d1f4c7249e0dd6e5d670bf266f5e1a
Moumy Ndiaye <e2177120@cmaisonneuve.qc.ca>
2023-12-09 00:53:07 -0500

    fin du pied de page

--- src/app/composants/footer.tsx
+++ src/app/composants/footer.tsx
@@ -3,6 +3,7 @@ import '../styles/footer.scss'
 export default function Footer() {
   return (
     <footer className='piedDePage'>
+      <div className='deco-pied-page'><img src="./svg/deco-pied-page.svg" alt="" /></div>
       <div className='contacte'>
         <h2>contacte</h2>
         <p>moumyndiaye@outlook.com</p>
@@ -11,7 +12,7 @@ export default function Footer() {
         <a href="https://www.linkedin.com/in/moumy-ndiaye-31988b290/"><img src="./icones/linkedin.png" alt="linkedin" /></a>
         <a href="https://github.com/NMoumy"><img src="./icones/github.png" alt="github" /></a>
       </div>
-      <p>© 2023 Moumy</p>
+      <p>© 2023 Moumy Ndiaye | Tous droits réservés</p>
 
     </footer>
   )
